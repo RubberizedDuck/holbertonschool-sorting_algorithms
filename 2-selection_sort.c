827e9bb33bbba5c1012f3a3f5468ae782a03065e
Gotmished <5002@holbertonschool.com>
2022-08-22 22:48:21 -0700

    Initial commit task two: implementing selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the selection sort algorithm
+ * @array: the array
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, min_i, next_i;
+
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+	i = 0;
+	while (i < size - 1)
+	{
+		min_i = i;
+		next_i = i + 1;
+		while (next_i < size)
+		{
+			if (array[next_i] < array[min_i])
+			{
+				min_i = next_i;
+			}
+			next_i = next_i + 1;
+		}
+
+		if (i != min_i)
+		{
+			temp = array[i];
+			array[i] = array[min_i];
+			array[min_i] = temp;
+			print_array(array, size);
+		}
+		i = i + 1;
+	}
+}
