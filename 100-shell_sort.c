5cc24614562341db77b80f30999165105c32def5
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-24 19:02:25 -0700

    Sorts an array using the shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array using the shell sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	/* starts with a big gap and then reduces the gap */
+	gap = size / 2;
+	while (gap > 0)
+	{
+		i = gap;
+		while (i < size)
+		{
+			temp = array[i];
+			j = i;
+			while(j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j = j - gap;
+			}
+			array[j] = temp;
+			i = i + 1;
+		}
+		gap = gap / 2;
+	}
+	print_array(array, size);
+}
