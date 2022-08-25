9e8f9bda2d1d9fc3655bc280cb70999dcd2c9b29
RubberizedDuck <dyl_2536@hotmail.com>
2022-08-24 20:59:22 -0700

    Fix: Create swap_int, rearrange code to match task

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,35 +1,49 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * shell_sort - Sorts an array using the shell sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  */
-
 void shell_sort(int *array, size_t size)
 {
 	size_t gap, i, j;
-	int temp;
 
-	/* starts with a big gap and then reduces the gap */
-	gap = size / 2;
-	while (gap > 0)
+	if (array == NULL || size < 2)
+		return;
+
+	gap = 1;
+	while (gap < (size / 3))
+		gap = gap * 3 + 1;
+
+	while (gap >=1)
 	{
 		i = gap;
 		while (i < size)
 		{
-			temp = array[i];
 			j = i;
-			while (j >= gap && array[j - gap] > temp)
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				array[j] = array[j - gap];
+				swap_ints(array + j, array + (j - gap));
 				j = j - gap;
 			}
-			array[j] = temp;
 			i = i + 1;
 		}
-		gap = gap / 2;
 		print_array(array, size);
+		gap = gap / 3;
 	}
-	print_array(array, size);
 }
